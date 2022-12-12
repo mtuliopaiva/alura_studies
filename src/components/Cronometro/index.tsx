aed002fa64d97e44ef654d43a6b624f2aca402ea
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-12-12 17:26:20 -0300

    Manipulando a seleção e usando useEffect 2

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -3,7 +3,7 @@ import Relogio from "./Relogio";
 import style from './Cronometro.module.scss'
 import { tempoParaSegundos } from "../../common/utils/time";
 import { ITarefas } from "../../types/tarefas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
     selecionado:ITarefas | undefined
@@ -11,11 +11,13 @@ interface Props {
 
 export function Cronometro({selecionado}:Props) {
     const [tempo, setTempo] = useState<number>();
-    if (selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
-    }
 
-
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+    
+      },[selecionado])
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>
